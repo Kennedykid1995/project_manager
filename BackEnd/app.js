23e7c58cc0fccec9ddcb52d7a82e1c8d3e3cdd00
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2020-04-27 16:02:17 -0500

    started first query

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphQLHttp = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
-})
+app.use('/graphql', graphQLHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation{
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['event', 'event1', 'event2']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(4000); 
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Backend Folder
 Whenever we make a new branch on Github do this command:
 git pull --all (to get the new branch)
 git checkout (branch name) (to switch to that branch)
+
+
