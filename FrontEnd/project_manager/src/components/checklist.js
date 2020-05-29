6dadb8520588b9e66230ae7547f253f43fa3e4fb
skennedy93 <sean47900@gmail.com>
2020-05-29 13:19:51 -0400

    put in basic component structure

---
+++ FrontEnd/project_manager/src/components/checklist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class checklist extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
